45e89bc62a9eb675da3c9582d65d4bb0d75a2f2d
Kupri4 <ilyakyp13rus@gmail.com>
2022-05-15 16:53:32 +0300

    about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React from "react";
+
+import computer from "../assets/icons/computer.svg";
+import repair from "../assets/icons/repair.svg";
+import api from "../assets/icons/api.svg";
+import algo from "../assets/icons/algo.svg";
+import puzzle from "../assets/icons/puzzle.svg";
+import Skillcard from "./Skillcard";
 
 const About = () => {
-    return (
-        <div>This is about page</div>
-    )
-}
+  const skills = [
+    {
+      icon: computer,
+      title: "Frontend Development",
+      content:
+        "I can build a beautiful and scalable SPA using HTML, CSS and Ract.js",
+    },
+    {
+      icon: repair,
+      title: "Backend Development",
+      content: "Handle database, server, api using and SQLite",
+    },
+    {
+      icon: api,
+      title: "API Development",
+      content: "I can develop robust REST API using django-rest-api",
+    },
+    {
+      icon: algo,
+      title: "Competitive Coder",
+      content: "A daily problem solver in HackerRank and Leetcode",
+    },
+    {
+      icon: puzzle,
+      title: "UI/UX designer",
+      content: "Minimalistic user interface using figma and framer",
+    },
+  ];
+
+  return (
+    <div className="about">
+      <div className="container">
+        <div className="about__container">
+          <h6 className="about__intro">
+            I describe myself as someone who's persistant, a quick learner and
+            lovaes problem solving by using simple and scalable solutions.
+          </h6>
+
+          <div className="skill-cards__container">
+            <h6 className="about__heading">What I offer</h6>
+
+            <div className="row">
+              {skills.map((skill) => (
+                <Skillcard skill={skill} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default About;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,49 +1,58 @@
 import react from "react";
-import myghtycoder from '../assets/mighty-coder.svg';
-import tie from '../assets/icons/tie.svg';
-import facebook from '../assets/icons/facebook.svg';
-import instagramm from '../assets/icons/instagram.svg'
-import github from '../assets/icons/github.svg';
-import pin from '../assets/icons/pin.svg';
-import resume from '../assets/resume.pdf'
-
-
-
+import myghtycoder from "../assets/mighty-coder.svg";
+import tie from "../assets/icons/tie.svg";
+import facebook from "../assets/icons/facebook.svg";
+import instagramm from "../assets/icons/instagram.svg";
+import github from "../assets/icons/github.svg";
+import pin from "../assets/icons/pin.svg";
+import resume from "../assets/resume.pdf";
 
 const Sidebar = () => {
+  const handleEmailMe = () => {
+    window.open("mailto:ilya-kyp@yandex.ru");
+  };
 
-    const handleEmailMe = () => {
-        window.open("mailto:ilya-kyp@yandex.ru")
-    }
-
-    return (
-        <div className="sidebar">
-            <img src={myghtycoder} alt="avatar" className="sidebar__avatar" />
-            <div className="sidebar__name">Ilya <span>Kupriyanov</span></div>
-            <div className="sidebar__item sidebar__title">C# Developer</div>
-            <a href={resume} download="resume.pdf">
-                <div className="sidebar__item">
-                    <img src={tie} alt="resulme" className="sidebar__icon" />
-                    Download Resume
-                </div>
-            </a>
-            <figure className="sidebar__social-icons sidebar__item">
-                <a href=""><img src={facebook} alt="facebook" className="sidebar__icon" /></a>
-                <a href=""><img src={instagramm} alt="instagramm" className="sidebar__icon" /></a>
-                <a href=""><img src={github} alt="github" className="sidebar__icon" /></a>
-            </figure>
-            <div className="contacts">
-                <div className="sidebar__location">
-                    <img src={pin} alt="location" className="sidebar__icon" />
-                    Zubova-Polyana, Mordovia
-                </div>
-
-                <div className="sidebar__item">ilya-kyp@yandex.ru</div>
-                <div className="sidebar__item">+7 (999) 888 77-66</div>
-            </div>
-            <div className="sidebar__item sidebar__email bkg-gradient" onClick={handleEmailMe}>Email me</div>
+  return (
+    <div className="sidebar">
+      <img src={myghtycoder} alt="avatar" className="sidebar__avatar" />
+      <div className="sidebar__name">
+        Ilya <span>Kupriyanov</span>
+      </div>
+      <div className="sidebar__item sidebar__title">C# Developer</div>
+      <a href={resume} download="resume.pdf">
+        <div className="sidebar__item">
+          <img src={tie} alt="resulme" className="sidebar__icon" />
+          Download Resume
         </div>
-    )
-}
+      </a>
+      <figure className="sidebar__social-icons sidebar__item">
+        <a href="">
+          <img src={facebook} alt="facebook" className="sidebar__icon" />
+        </a>
+        <a href="">
+          <img src={instagramm} alt="instagramm" className="sidebar__icon" />
+        </a>
+        <a href="">
+          <img src={github} alt="github" className="sidebar__icon" />
+        </a>
+      </figure>
+      <div className="contacts">
+        <div className="sidebar__location sidebar__item">
+          <img src={pin} alt="location" className="sidebar__icon" />
+          Zubova-Polyana, Mordovia
+        </div>
 
-export default Sidebar
+        <div className="sidebar__item">ilya-kyp@yandex.ru</div>
+        <div className="sidebar__item">+7 (999) 888 77-66</div>
+      </div>
+      <div
+        className="sidebar__item sidebar__email bkg-gradient"
+        onClick={handleEmailMe}
+      >
+        Email me
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
